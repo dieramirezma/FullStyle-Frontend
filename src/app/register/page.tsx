1c623ce72d0cc44cd41e7e1b51d858329b0e4a4b
bforerob <bforerob@unal.edu.co>
2024-12-30 09:07:33 -0500

    Basic register type page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+import Fullstyle from '@/components/icons/fullstyle'
+import RegisterNavBar from '@/components/register-nav-bar'
+import TypeRegisterBox from '@/components/type-register-box'
+
+export default function registerPage () {
+  return (
+    <div>
+      <RegisterNavBar />
+      <div className="h-full flex items-center place-content-around px-20 pt-40">
+        <Fullstyle />
+        <TypeRegisterBox />
+      </div>
+    </div>
+
+  )
+}
