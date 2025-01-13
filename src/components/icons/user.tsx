930b5f7a33f860531618db807756fd65c5463198
bforerob <bforerob@unal.edu.co>
2025-01-13 18:43:00 -0500

    asdasd

---
+++ src/components/icons/user.tsx
@@ -0,0 +1,7 @@
+import { type SVGProps } from 'react'
+
+export function UserIcon (props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="none" stroke="#EBA745" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 20v-1a7 7 0 0 1 7-7v0a7 7 0 0 1 7 7v1m-7-8a4 4 0 1 0 0-8a4 4 0 0 0 0 8"/></svg>
+  )
+}
