54afc4935dc9ab7d61cbfc1467567685fb19369f
dieramirezma <dieramirezma@unal.edu.co>
2025-02-05 19:51:21 -0500

    feat(): enhance layout with image and improve form presentation in login/register

--- src/app/register/customer/page.tsx
+++ src/app/register/customer/page.tsx
@@ -1,14 +1,24 @@
-import Fullstyle from '@/components/icons/fullstyle'
 import RegisterCustomerForm from '@/components/register-customer-form'
 import RegisterNavBar from '@/components/register-nav-bar'
+import Image from 'next/image'
 
 export default function registerClientPage () {
   return (
     <div>
       <RegisterNavBar />
-      <div className="h-full flex items-center place-content-around px-20">
-        <Fullstyle />
-        <RegisterCustomerForm></RegisterCustomerForm>
+      <div className="h-full flex items-stretch justify-center px-20 gap-16">
+        <div className="flex-1 max-w-[50%] flex items-center">
+          <Image
+            src="/images/login.png"
+            alt="Login"
+            width={600}
+            height={500}
+            className="rounded-xl h-full object-cover"
+          />
+        </div>
+        <div className="w-[400px] flex items-center">
+          <RegisterCustomerForm />
+        </div>
       </div>
     </div>
   )
