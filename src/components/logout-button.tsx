a23954c8d6c12793285e96cf911d90d5bb7c8afc
Diego Nicolás Ramírez <dieramirezma@unal.edu.co>
2025-02-06 01:39:13 -0500

    Revert "feat(owner): implement dashboard and services view"

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -2,18 +2,12 @@
 
 import { LogOut } from 'lucide-react'
 import { signOut } from 'next-auth/react'
-import { Button } from './ui/button'
-import { cn } from '@/lib/utils'
 
 function LogoutButton () {
   return (
-    <Button
-      variant='ghost'
-      className={cn('h-7 w-7 [&_svg]:size-6')}
-      onClick={async () => { await signOut({ callbackUrl: '/login' }) }}
-    >
-      <LogOut size={30} />
-    </Button>
+    <button onClick={async () => { await signOut({ callbackUrl: '/login' }) }}>
+      <LogOut size={30} color='#EBA745'/>
+    </button>
   )
 }
 
